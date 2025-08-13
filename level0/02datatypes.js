cd8c78957c37fae09619963b436403e05c7708e5
rishabkumar03 <rishabkumar3.2005@gmail.com>
2025-08-13 20:00:31 +0530

    Enhance documentation in 02datatypes.js with additional comments on JavaScript features

--- level0/02datatypes.js
+++ level0/02datatypes.js
@@ -1,6 +1,22 @@
 "use strict"; // treat all JS code as newer version
+/*
+
+- Prevents us from using undeclared variables.
+- Stops us from using unsafe keywords ("with, eval" in unsafe ways).
+- Throws more errors instead of silently failing (helps in catching bugs early)
+- Reserve certain keywords for future javascr
+
+ */
+
 
 //alert (3+3) // we are using nodejs, not any browser
+/*
+
+- alert() is a part of web browser APIs, not a part of core JavaScript language.
+- In browser, it is used to show pop-up message to the user.
+- Node.js runs JavaScript outside the browser, that's why it doesn't have windows or alert box.
+
+*/
 
 console.log(3
     +
@@ -10,7 +26,7 @@ console.log(3
 console.log("6kku")
 
 // number => 2 to power 4
-// bigint (YHi use krna hai mujhe hackathon mein)
+// bigint (Yhi use krna hai mujhe hackathon mein)
 // string => ""
 // boolean => true/false
 // null => standalone value
